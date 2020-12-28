6ae6e3b659039ead4167abe84e2f78702eb2040d
WesGatner <wesgtr@gmail.com>
2020-12-28 11:37:07 -0300

    fix 01

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import routes from './routes';
-import './database';
+import routes from'./routes';
 
 class App {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store)
+routes.get('/', (req, res) => {
+  return res.json({message: 'Hello USes'});
+})
 
 export default routes;
