53ca9ea87e5efbaafd1cd842a9e2175b8c0bff02
WesGatner <wesgtr@gmail.com>
2020-12-30 15:37:36 -0300

    checking error

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import Appointment from '../models/Appointment';
-import {startOfHour, parseISO, isBefore } from  'date-fns';
+import {startOfHour, parseISO, isBefore, format, subHours } from  'date-fns';
+import pt from 'date-fns/locale/pt-BR'
 import User from '../models/User';
 import File from '../models/File';
 import * as Yup from 'yup';
+import Notification from '../schemas/Notifications';
 
 class AppointmentController {
   async index(req,res) {
@@ -41,6 +43,10 @@ class AppointmentController {
 
     const { provider_id, date } = req.body;
 
+    // if (req.userId === provider_id) {
+    //   return res.status(400).json({ error: 'User and Provider are the same' });
+    // }
+
     /**
      * check if provider id is a provider
      */
@@ -78,6 +84,45 @@ class AppointmentController {
       provider_id,
       date
     });
+
+    /**
+     * Notify appointment provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formatedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      {locale: pt}
+      );
+
+    await Notification.create({
+      content: `Novo agendamente de ${user.name} para ${formatedDate}`,
+      user: provider_id,
+    })
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment."
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: "You can only cancel appointments 2 hours in advance."
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import FileController from'./app/controllers/FileController';
 import ProviderController from'./app/controllers/ProviderController';
 import AppointmentController from'./app/controllers/AppointmentController';
 import ScheduleController from'./app/controllers/ScheduleController';
+import NotificationController from'./app/controllers/NotificationController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -24,11 +25,15 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
-routes.post('/files', upload.single('file'), FileController.store);
-
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
