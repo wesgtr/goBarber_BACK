d2d593e6d32887ceeda067afcc3552b1c246a401
WesGatner <wesgtr@gmail.com>
2020-12-30 14:32:19 -0300

    mongo db

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import Appointment from '../models/Appointment';
-import {startOfHour, parseISO, isBefore } from  'date-fns';
+import {startOfHour, parseISO, isBefore, format } from  'date-fns';
+import pt from 'date-fns/locale/pt-BR'
 import User from '../models/User';
 import File from '../models/File';
 import * as Yup from 'yup';
+import Notification from '../schemas/Notifications';
 
 class AppointmentController {
   async index(req,res) {
@@ -78,6 +80,22 @@ class AppointmentController {
       provider_id,
       date
     });
+
+    /**
+     * Notify appointment provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formatedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      {locale: pt}
+      );
+
+    await Notification.create({
+      content: `Novo agendamente de ${user.name} para ${formatedDate}`,
+      user: provider_id,
+    })
+
 
     return res.json(appointment);
   }
