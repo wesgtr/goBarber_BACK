dabf9c3d1e61476ca722d5a8f45306960dab9d79
WesGatner <wesgtr@gmail.com>
2020-12-30 15:29:44 -0300

    Revert "notifications"
    This reverts commit 8febe59d75561b32d0d9f5c7fbb9cc1e28aa3c90.

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -1,33 +1,19 @@
-import Notification from '../schemas/Notifications';
 import User from '../models/User';
+import File from '../models/File';
 
 class NotificationController {
   async index(req,res) {
+    const providers = await User.findAll({
+      where: { provider: true },
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [{
+        model: File,
+        as: 'avatar',
+        attributes: ['name','path','url']
+      }]
+    })
 
-    const isProvider = await User.findOne({
-      where: { id: req.userId, provider: true }
-    });
-
-    if(!isProvider) {
-      return res.status(401).json({
-        error: 'Only providers can load notifications'});
-    }
-
-    const notifications = await Notification.find({
-      user: req.userId
-    }).sort({createdAt: 'desc'}).limit(20)
-
-    return res.json(notifications);
-  }
-
-  async update(req, res) {
-    const notification = await Notification.findByIdAndUpdate(
-      req.params.id,
-      { read: true },
-      { new: true}
-    )
-
-    return res.json(notification);
+    return res.json(providers);
   }
 }
 
