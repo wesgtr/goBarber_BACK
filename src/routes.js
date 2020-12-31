4d83bfd1eee647e793509172d6bc1e7131e8fb56
WesGatner <wesgtr@gmail.com>
2020-12-31 12:59:26 -0300

    restarting project

--- src/routes.js
+++ src/routes.js
@@ -24,11 +24,11 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
-routes.post('/files', upload.single('file'), FileController.store);
-
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
